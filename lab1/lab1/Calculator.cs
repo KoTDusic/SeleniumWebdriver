73e1bde2dc0d82bb7d1a6f6be5d30538764bb9e1
KoTDusic <kotdusic@gmail.com>
2016-09-13 16:21:20 +0300

    first commit

---
+++ lab1/lab1/Calculator.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab1
+{
+    class Calculator
+    {
+        double result;
+        public Calculator()
+        {
+            result = 0;
+        }
+
+        public double calculate(Char operation, double v1, double v2)
+        {
+            switch (operation)
+    
+        {
+                case '+':
+                    return v1 + v2;
+                case '-':
+                    return v1 - v2;
+                case '*':
+                    return v1 * v2;
+                case '/':
+                    return v1 / v2;
+                default:
+                    Exception e = new Exception("введена неизвестная операция");
+                    throw e;
+            }
+        }
+
+        public string FormatResult(Char operation, double v1, double v2)
+        {
+            String res="";
+            try
+            {
+                result = calculate(operation, v1, v2);
+                res += String.Format("результат вычисления {0} {1} {2} = {3}", v1, operation, v2, result);
+            }
+            catch (Exception e)
+            {
+                res += String.Format("неверные входные данные, произошла ошибка:{0}", e.Message);
+            }
+            return res;
+        }
+    }
+}
---
+++ lab1/lab1/CalculatorController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab1
+{
+    class CalculatorController
+    {
+        Calculator calculator;
+        double value1, value2;
+        char operation;
+        const String description = "калькулятор v0.1, доступные операции: сложение(+),вычитание(-),умножение(*), деление(/)";
+
+        public CalculatorController()
+        {
+            calculator = new Calculator();
+            Console.WriteLine(description);
+        }
+
+        public void ReadValuesAndCalculate()
+        {
+            Console.Write("ведите операцию");
+            operation = Convert.ToChar(Console.ReadLine()); //.ReadKey().KeyChar
+            Console.WriteLine();
+            Console.Write("операция принята");
+            Console.WriteLine();
+            while (true)
+            {
+                try
+                {
+                    Console.Write("ведите первый операнд: ");
+                    value1 = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine();
+                    break;
+                }
+                catch 
+                {
+                    Console.WriteLine("ошибка в вводе первого операнда, вводить число, отделяя дробную часть точкой");
+    
+            }
+            }
+            while (true)
+            {
+                try
+                {
+                    Console.Write("ведите второй операнд: ");
+                    value2 = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine();
+                    break;
+                }
+                catch
+                {
+                    Console.WriteLine("ошибка в вводе второго операнда, вводить число, отделяя дробную часть точкой");
+    
+            }
+            }
+            Console.WriteLine(calculator.FormatResult(operation, value1, value2));
+        }
+    }
+}
