6c69c26527d1572dcc01c4bd84563a41ebd38dd4
Prokopovich <kotdusic@gmail.com>
2016-10-09 16:49:14 +0300

    2 test

--- SeleniumTest/SeleniumTest/Class1.cs
+++ SeleniumTest/SeleniumTest/Class1.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
-
+using System.IO;
 namespace SeleniumTest
 {
     [TestFixture]
@@ -30,6 +30,16 @@ namespace SeleniumTest
             IWebElement login_button = driver.FindElement(By.Id("welcome_box_sign_in_button"));
             login_button.Click();
         }
+        public void LoadAvatar()
+        {
+            IWebElement edit_profile_button = driver.FindElement(By.XPath(".//*[@id='profile_account_actions']/ul/li[1]/a"));
+            edit_profile_button.Click();
+            IWebElement upload_button = driver.FindElement(By.Id("user_uploaded_data"));
+            string avatar = Path.GetFullPath("Images/1.jpg");
+            upload_button.SendKeys(avatar);
+            IWebElement submit_button = driver.FindElement(By.XPath(".//*[@id='edit_user']/dl/dt[10]/input"));
+            submit_button.Click();
+        }
         [TearDown]
         public void Teardown()
         {
@@ -41,6 +51,14 @@ namespace SeleniumTest
             Login("testlabatestlaba@gmail.com", "12345");
             IWebElement user_name=driver.FindElement(By.XPath(".//*[@id='nav_welcome_box']/li[1]/a/span[2]"));
             Assert.AreEqual("my_first_test", user_name.Text);
+        }
+        [Test]
+        public void Test2()
+        {
+            Login("testlabatestlaba@gmail.com", "12345");
+            driver.Navigate().GoToUrl("http://www.kongregate.com/accounts/my_first_test");
+            LoadAvatar();
+            Assert.Pass();
         }
     }
 }
